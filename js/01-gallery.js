ab793d0d5da1a87d9c1b32bf8ee863b2ee7c9515
Viktoriya Ovcharenko <vickymmm2012@gmail.com>
2022-09-19 12:04:31 +0200

    Done task #1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,4 +3,60 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
+const galleryContainer = document.querySelector('.gallery');
+
+/*Створюємо галерею*/
+
+function createGalleryItems ({ preview, original, description}) {
+    return `
+      <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+        </a>
+      </div>
+    `;
+}
+
+const addGalleryItems = galleryItems.map(createGalleryItems).join('');
+galleryContainer.insertAdjacentHTML ('beforeend', addGalleryItems);
+
+/*Додаємо подію кліка на картинку i появи модального вікна*/
+
+
+galleryContainer.addEventListener("click", openModalPicture);
+
+function openModalPicture(event) {
+    event.preventDefault();
+
+    if (event.target.nodeName !== 'IMG') {
+        return
+    };
+
+    const instance = basicLightbox.create((`<img src="${event.target.getAttribute('data-source')}" width="800" height="600">`),
+    
+    {
+        onShow: () => {
+        document.addEventListener('keydown', onEscapeClose);
+    }, 
+
+       onClose: () => {
+        document.removeEventListener('keydown', onEscapeClose);
+    }
+    });
+
+    instance.show()
+
+    function onEscapeClose(event) {
+        if(event.key === "Escape") {
+            instance.close();
+        }
+    }
+};
+
+
+
+
+
+
+
 
